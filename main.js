e13aa66fb2e2d8dc3d8dfb6ebd75857059ef61fe
dianagiangregorio <diana.giangregorio@gmail.com>
2024-10-28 14:29:26 +0100

    feat: added promo code logic

--- main.js
+++ main.js
@@ -7,12 +7,17 @@ const calculatorForm = document.getElementById('calculator-form');
 const jobType = document.getElementById("job-type");
 const promoCode = document.getElementById("promo-code");
 
+//creare un array con i codici promozionali validi
+
+const validPromoCode = ['YHDNU32',
+    'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
+
 //invio il form e leggo i dati
 calculatorForm.addEventListener('submit', function (event) {
     event.preventDefault();
     
     console.log("tipo di lavoro:", jobType.value);
-    console.log(promoCode.value)
+    console.log(promoCode.value);
 
     //creo le variabili per il numero di ore per lavoro e il prezzo orario di ogni lavoro
 
@@ -34,5 +39,24 @@ calculatorForm.addEventListener('submit', function (event) {
     else if (jobType.value === '3'){
         let jobPrice = jobHours * analysisHourPrice;
         console.log(jobPrice);
+    }
+
+    //controllare se è presente un codice sconto: se c'è applicare sconto, se non c'è stampare messaggio
+
+    if (promoCode.value === ''){
+        console.log('Non hai inserito un codice sconto')
+    }
+    else {
+        for (let i = 0; i < validPromoCode.length; i++ ){
+            if (validPromoCode.includes(promoCode.value)) {
+                const finalPrice = (jobPrice * 25)/100
+                console.log(finalPrice)
+            }
+            else {
+                console.log('Il codice inserito non è corretto')
+                const finalPrice = jobPrice
+                console.log(finalPrice)
+            }
+        }
     }
 })
