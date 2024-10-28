a6cd65845549e6fad48fdd1db97e7191b6a1b98a
dianagiangregorio <diana.giangregorio@gmail.com>
2024-10-28 14:04:34 +0100

    feat: added price logic

--- main.js
+++ main.js
@@ -13,4 +13,26 @@ calculatorForm.addEventListener('submit', function (event) {
     
     console.log("tipo di lavoro:", jobType.value);
     console.log(promoCode.value)
+
+    //creo le variabili per il numero di ore per lavoro e il prezzo orario di ogni lavoro
+
+    const jobHours = 10
+    const backendHourPrice = 20.50
+    const frontendHourPrice = 15.30
+    const analysisHourPrice = 33.60
+
+    //logica per scoprire il prezzo totale di ogni lavoro
+
+    if (jobType.value === '1'){
+        let jobPrice = jobHours * backendHourPrice;
+        console.log(jobPrice);
+    }
+    else if (jobType.value === '2'){
+        let jobPrice = jobHours * frontendHourPrice;
+        console.log(jobPrice);
+    }
+    else if (jobType.value === '3'){
+        let jobPrice = jobHours * analysisHourPrice;
+        console.log(jobPrice);
+    }
 })
