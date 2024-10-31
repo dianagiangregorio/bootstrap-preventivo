ac63641d7af8972b057af4a15975a997cee582f1
dianagiangregorio <diana.giangregorio@gmail.com>
2024-10-31 10:32:30 +0100

    feat: added functions

--- main.js
+++ main.js
@@ -1,62 +1,78 @@
-//creare una variabile con il form
+//creo una variabile con il form
 
 const calculatorForm = document.getElementById('calculator-form');
 
-//creare delle variabili per i campi del form
+//creo delle variabili per i campi del form
 
 const jobType = document.getElementById("job-type");
-const promoCode = document.getElementById("promo-code");
+const codeToCheck = document.getElementById("promo-code");
 
-//creare un array con i codici promozionali validi
+//creo un array con i codici promozionali validi
 
 const validPromoCode = ['YHDNU32',
     'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
 
-//invio il form e leggo i dati
+//creo le variabili per il numero di ore per lavoro e il prezzo orario di ogni lavoro
+
+const jobHours = 10
+const backendHourPrice = 20.50
+const frontendHourPrice = 15.30
+const analysisHourPrice = 33.60
+
+
+//all'invio del form, leggo i dati e calcolo il prezzo del lavoro
+
 calculatorForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    
-    console.log("tipo di lavoro:", jobType.value);
-    console.log(promoCode.value);
 
-    //creo le variabili per il numero di ore per lavoro e il prezzo orario di ogni lavoro
-
-    const jobHours = 10
-    const backendHourPrice = 20.50
-    const frontendHourPrice = 15.30
-    const analysisHourPrice = 33.60
-
-    //logica per scoprire il prezzo totale di ogni lavoro
+    //calcolo il prezzo orario per il tipo di lavoro selezionato dall'utente
 
     if (jobType.value === '1'){
-        let jobPrice = jobHours * backendHourPrice;
-        console.log(jobPrice);
+        jobPrice(jobHours, backendHourPrice);
     }
     else if (jobType.value === '2'){
-        let jobPrice = jobHours * frontendHourPrice;
-        console.log(jobPrice);
-    }
-    else if (jobType.value === '3'){
-        let jobPrice = jobHours * analysisHourPrice;
-        console.log(jobPrice);
-    }
-
-    //controllare se è presente un codice sconto: se c'è applicare sconto, se non c'è stampare messaggio
-
-    if (promoCode.value === ''){
-        console.log('Non hai inserito un codice sconto')
+        jobPrice(jobHours, frontendHourPrice);
     }
     else {
-        for (let i = 0; i < validPromoCode.length; i++ ){
-            if (validPromoCode.includes(promoCode.value)) {
-                const finalPrice = (jobPrice * 25)/100
-                console.log(finalPrice)
-            }
-            else {
-                console.log('Il codice inserito non è corretto')
-                const finalPrice = jobPrice
-                console.log(finalPrice)
-            }
-        }
+        jobPrice(jobHours, analysisHourPrice);
     }
+        
+    //controllo se l'utente ha inserito un codice sconto e calcolo il prezzo finale
+
+    if (promoCode(validPromoCode, codeToCheck.value)) {
+        const finalPrice = totalJobPrice - ((totalJobPrice * 25)/100);
+        console.log(finalPrice);
+    }
+    else if (codeToCheck.value === '') {
+        const finalPrice = totalJobPrice;
+        console.log(`Non hai inserito il codice ${finalPrice}`);
+    }
+    else {
+        const finalPrice = totalJobPrice
+        console.log(`Il codice inserito non è corretto ${finalPrice}`)
+    }
+
 })
+
+//FUNZIONI
+
+
+/**
+ * calcola il prezzo totale di un determinato lavoro
+ * @param {number} hoursToJob
+ * @param {number} pricePerHour
+ * @returns {number}
+ */
+function jobPrice (hoursToJob, pricePerHour) {
+    return totalJobPrice = hoursToJob * pricePerHour
+}
+
+/**
+ * funzione per controllare se il codice sconto inserito dall'utente è valido
+ * @param {Array} arrayPromoCode
+ * @param {string} codeToCheck
+ * @returns {boolean}
+ */
+function promoCode (arrayPromoCode, codeToCheck) {
+    return arrayPromoCode.includes(codeToCheck)
+}
